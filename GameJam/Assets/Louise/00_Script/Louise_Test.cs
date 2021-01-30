7b87d32fc24ad65376f8ed3faa4f9b74ca54bd36
Home\louise <>
2021-01-30 09:56:04 +0800

    新增 ColorBlindnessMode

---
+++ GameJam/Assets/Louise/00_Script/Louise_Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Louise_Test : MonoBehaviour
+{
+    public Transform volume;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        volume.position = transform.position;
+    }
+}
