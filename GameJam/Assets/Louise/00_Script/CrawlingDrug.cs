101ee0c6d225b128a94f245eb1b037a0e658dadf
Home\louise <>
2021-01-30 16:02:48 +0800

    藥頭賣藥

---
+++ GameJam/Assets/Louise/00_Script/CrawlingDrug.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class CrawlingDrug : Interactable
+{
+    public FirstPersonController controller;
+    public CharacterController characterController;
+    void Start()
+    {
+        controller = FindObjectOfType<FirstPersonController>();
+        characterController = controller.GetComponent<CharacterController>();
+
+    }
+    public override void Use()
+    {
+        base.Use();
+        characterController.radius = 0f;
+        characterController.height = 0.3f;
+        controller.m_WalkSpeed = 0.5f;
+        controller.m_RunSpeed = 0.5f;
+        controller.isCrawling = true;
+        StartCoroutine(Recovery());
+    }
+
+    public IEnumerator Recovery()
+    {
+        yield return new WaitForSeconds(10f);
+        characterController.radius = 0.5f;
+        characterController.height = 1.8f;
+        controller.m_WalkSpeed = 2f;
+        controller.m_RunSpeed = 5f;
+        controller.isCrawling = false;
+        Debug.Log("恢復");
+    }
+}
---
+++ GameJam/Assets/Louise/00_Script/Drug.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Rigidbody),typeof(BoxCollider))]
+public class Drug : Interactable
+{
+    public Volume volume;
+    public VolumeProfile volumeProfile;
+    public Rigidbody rigidbody;
+    public Louise_Test louise_Test;
+    void Start()
+    {
+        volume = FindObjectOfType<Volume>();
+        rigidbody = GetComponent<Rigidbody>();
+        louise_Test = FindObjectOfType<Louise_Test>();
+        rigidbody.useGravity = false;
+    }
+    public override void Use()
+    {
+        base.Use();
+        volume.sharedProfile = volumeProfile;
+        louise_Test.timerEffect = 10f;
+    }
+}
