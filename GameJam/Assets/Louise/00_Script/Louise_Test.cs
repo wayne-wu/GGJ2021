0b5fd854d994b55373988e4144a3731edd687ae1
Home\louise <>
2021-01-30 10:48:56 +0800

    ColorBlind完成

---
+++ GameJam/Assets/Louise/00_Script/Louise_Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Louise_Test : MonoBehaviour
+{
+    public Transform volumeTrans;
+    public Volume volume;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        //volume.sharedProfile
+    }
+
+    void Update()
+    {
+        volumeTrans.position = transform.position;
+    }
+}
